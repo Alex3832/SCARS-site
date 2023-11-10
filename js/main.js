727d60b34bae2959c47fda356e1b03a58cc522d9
Alex <alex@Alexs-MacBook-Pro.local>
2023-11-10 10:11:16 -0600

    Added burger

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-function uploadImage() {
-	
-}
+  document.querySelector('input[type="file"]').addEventListener('change', function() {
+      if (this.files && this.files[0]) {
+      	  let imageContainer = document.getElementById('drop-image');
+      	  imageContainer.innerHTML = "<img id='uploaded-image'>";
+          let img = document.getElementById('uploaded-image');
+          img.onload = () => {
+              URL.revokeObjectURL(img.src);  // no longer needed, free memory
+          }
+
+          img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+      }
+  })
+      
