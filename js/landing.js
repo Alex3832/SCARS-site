39d9844fda3de8d725923fbd2b04ec24034d05c4
Alex <alex@Alexs-MacBook-Pro.local>
2023-11-14 13:52:53 -0600

    Eddited initial landing page

---
+++ js/landing.js
@@ -0,0 +1,11 @@
+function mouseOver() {
+	document.getElementById("button").style.background = "orange";
+
+	document.getElementById("button").style.color = "black";
+}
+
+function mouseLeave() {
+	document.getElementById("button").style.background = "black";
+		document.getElementById("button").style.color = "orange";
+
+}
